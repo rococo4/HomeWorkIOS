f51342a4e98201573a8881109161fa18105f7ac2
rococo4 <90200936+rococo4@users.noreply.github.com>
2022-11-22 23:38:38 +0300

    Add files via upload

---
+++ HW_4/HW_2/AddNoteDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AddNoteDelegate.swift
+//  HW_2
+//
+//  Created by Lebedev A on 22.11.2022.
+//
+
+import Foundation
+
+protocol AddNoteDelegate {
+    func newNoteAdded(note: ShortNote)
+}
---
+++ HW_4/HW_2/ShortNode.swift
@@ -0,0 +1,12 @@
+//
+//  ShortNode.swift
+//  HW_2
+//
+//  Created by Lebedev A on 22.11.2022.
+//
+
+import Foundation
+
+struct ShortNote {
+    var text: String
+}
